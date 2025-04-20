71c422d2ae43cc92c7b4c02bf1d781372c246526
kalpesh95 <kalpeshlad95@gmail.com>
2025-04-20 19:42:34 +0530

    Saga Added

--- src/sagas/index.ts
+++ src/sagas/index.ts
@@ -1,9 +1,19 @@
-// src/sagas/index.ts
-import { all, fork } from 'redux-saga/effects';
-import  authSaga  from '../features/Login/saga';
+import { all } from 'redux-saga/effects';
 
+// Import your individual sagas
+import authSaga from '../features/Login/saga';
+import instructorSaga from '../features/Instructor/saga';
+import { courseSaga } from '../features/Course/saga';         // <- named!
+import {lectureSaga} from '../features/Lecture/saga';
+import {dashboardSaga} from '../features/Home/saga';
+
+// Combine all sagas here
 export default function* rootSaga() {
   yield all([
-    fork(authSaga)
+    authSaga(),
+    instructorSaga(),
+    courseSaga(),
+    lectureSaga(),
+    dashboardSaga(),
   ]);
 }
